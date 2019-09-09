ace30a4f12c004a1725339efd4d744413c57b3a1
In2Lab <in2lab@iMac-de-In2Lab.local>
2019-09-09 11:59:48 -0500

    Add Prediction pipeline

--- voice-detector.py
+++ voice-detector.py
@@ -11,16 +11,11 @@ import model
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import load_img
-
-
+from utils import load_id_label_map
+import time
 
 TARGET_NAMES = ["acelera", "arranca", "avanza", "derecha",
                 "pita", "izquierda", "frena", "detente", "retrocede", "gira"]
-
-parser = argparse.ArgumentParser(description="Pipeline for keywords predicts")
-parser.add_argument("--wait", type=int, default="", help="Waiting time")
-args = parser.parse_args()
-wait = args.wait
 
 
 recognizer = sr.Recognizer()
@@ -30,24 +25,31 @@ recognizer.pause_threshold = 0.3
 recognizer.dynamic_energy_adjustment_ratio = 2
 microphone = sr.Microphone()
 
-next_time = datetime.now() - timedelta(seconds=1)
-current_time = datetime.now().time()
 
 params = utils.yaml_to_dict('config.yml')
 params['data_dir'] = './microphone-dataset'
 params['save_audio_fragments'] = False
 params['model_dir'] = params['model_dir']
 
-
+LABELS = load_id_label_map(params)
 
 def load_model(params):
-    tf.keras.backend.clear_session()
-    width, height = params['image_shape']
-    inputs = tf.keras.layers.Input(shape=(width, height, 3))
-    net = model.ModelArchitecture(num_classes=params['num_classes'])
-    x = net(inputs, training=False)
-    return net
+  """
+  """
+  tf.keras.backend.clear_session()
+  width, height = params['image_shape']
+  inputs = tf.keras.layers.Input(shape=(width, height, 3))
+  net = model.ModelArchitecture(num_classes=params['num_classes'])
+  x = net(inputs, training=False)
+  return net
 
+
+def make_predictions(image):
+  predictions = NET.predict(image, batch_size=1)
+  print("Prediction", LABELS[np.argmax(predictions, axis=1)[0]])
+  return np.argmax(predictions, axis=1)
+
+# Instance Neural Network
 try:
   NET = load_model(params) 
   optimizer = tf.keras.optimizers.Adam(lr=params['learning_rate'])
@@ -55,13 +57,10 @@ try:
               'sparse_categorical_accuracy'])
   NET.load_weights(os.path.join(params['model_dir'], 'tf_ckpt'))
   print("[INFO] Model loaded")
-except :
+except Exception as e :
   print("[ERROR] Some error while try to load model")
+  print(e)
 
-def make_predictions(image):
-    predictions = NET.predict(image, batch_size=1)
-    print("Prediction", predictions)
-    return np.argmax(predictions, axis=1)
 
 
 def listen():
@@ -78,14 +77,16 @@ def listen():
     try:
       f.write(audio.get_wav_data())
       print('[INFO] Saving ', audio)
-    except:
+    except Exception as e :
       print("[ERROR] Some error in saved-audio script")
+      print(e)
 
   try:
     print("[INFO] Preprocesing audio script")
     preprocessing.generate_spectogram_images(params)
-  except :
+  except Exception as e :
     print("[ERROR]: Some error in preprocessing-audio script")
+    print(e)
   
 
   img = load_img(specgram_image)
@@ -98,12 +99,11 @@ def listen():
     prediction = make_predictions(img)
     print("[INFO] Predictions {}".format(prediction))
     print("[INFO] Talk!")
-  except :
-    print("[ERROR] We can`t predict ")
+    time.sleep(10)
+  except Exception as e:
+    print("[ERROR] We can`t predict, Try again ")
+    print(e)
 print("[INFO] Talk!")
 
 while True:
-  current_time = datetime.now()
-  if(current_time >= next_time):
-    listen()
-    next_time = datetime.now() + timedelta(seconds=wait)
+  listen()
